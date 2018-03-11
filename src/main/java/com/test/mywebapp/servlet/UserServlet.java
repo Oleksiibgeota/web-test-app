9b933e8367c53c478dda550fd1b120a06b89733f
Oleksii <bgeota@gmail.com>
2018-03-11 15:32:57 +0200

    add user create method

--- src/main/java/com/test/mywebapp/servlet/UserServlet.java
+++ src/main/java/com/test/mywebapp/servlet/UserServlet.java
@@ -1,6 +1,21 @@
 package com.test.mywebapp.servlet;
 
+import com.test.mywebapp.dao.mysql.MySqlUserDAO;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
+
+    private MySqlUserDAO userDAO = new MySqlUserDAO();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("firstname");
+        String lastName = req.getParameter("lastname");
+        userDAO.createUser(firstName, lastName);
+    }
 }
