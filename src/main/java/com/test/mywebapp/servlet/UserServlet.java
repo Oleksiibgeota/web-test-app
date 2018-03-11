aceea6a2202d87807f6222585c1196b8f79c8490
Oleksii <bgeota@gmail.com>
2018-03-11 15:35:00 +0200

    add list all users

--- src/main/java/com/test/mywebapp/servlet/UserServlet.java
+++ src/main/java/com/test/mywebapp/servlet/UserServlet.java
@@ -1,12 +1,14 @@
 package com.test.mywebapp.servlet;
 
 import com.test.mywebapp.dao.mysql.MySqlUserDAO;
+import com.test.mywebapp.dto.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class UserServlet extends HttpServlet {
 
@@ -24,5 +26,13 @@ public class UserServlet extends HttpServlet {
         String firstName = req.getParameter("firstname");
         String lastName = req.getParameter("lastname");
         userDAO.deleteUserByFirstNameAndLastName(firstName, lastName);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<User> users = userDAO.getAllUsers();
+        System.out.println("\nUser size: " + users.size() + "\n");
+        req.setAttribute("users", users);
+        req.getRequestDispatcher("mwa.jsp").forward(req, resp);
     }
 }
