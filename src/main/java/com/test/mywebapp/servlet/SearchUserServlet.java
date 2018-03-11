29002b2630f9feecb6b75dbcc17ef9046d605d19
Oleksii <bgeota@gmail.com>
2018-03-11 16:13:46 +0200

    create SearchUserServlet

---
+++ src/main/java/com/test/mywebapp/servlet/SearchUserServlet.java
@@ -0,0 +1,24 @@
+package com.test.mywebapp.servlet;
+
+import com.test.mywebapp.dao.mysql.MySqlUserDAO;
+import com.test.mywebapp.dto.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SearchUserServlet extends HttpServlet {
+    private MySqlUserDAO userDAO = new MySqlUserDAO();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("firstName");
+        if (firstName != null) {
+            User user = userDAO.getUserByName(firstName);
+            req.setAttribute("user", user);
+        }
+        req.getRequestDispatcher("search-user.jsp").forward(req, resp);
+    }
+}
